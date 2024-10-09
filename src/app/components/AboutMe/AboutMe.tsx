cad0888eccda5794bb51c3823e8c5b670749a741
Igor Wilian Faoro <igor.faoro17@gmail.com>
2024-10-09 10:55:03 -0300

    feat: explain main project

--- src/app/components/AboutMe/AboutMe.tsx
+++ src/app/components/AboutMe/AboutMe.tsx
@@ -9,7 +9,7 @@ export default function AboutMe({}: AboutMeProps) {
 
       <p>
         I am a full-stack developer with experience in various technologies,
-        including Node.js, Angular, React, .Net, and Java. With strong skills in
+        including Node.js, React, Angular, Java and C#. With strong skills in
         both backend and frontend development, I have the ability to create
         robust and scalable solutions to meet clients' needs. I have a
         problem-solving mindset and am always seeking to learn new technologies
--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -8,6 +8,21 @@ interface ProjectsProps {}
 export default function Projects({}: ProjectsProps) {
   return (
     <section className="space-y-4">
+      <p className="text-sm italic">
+        This{' '}
+        <a
+          href="https://github.com/igorwfaoro/igorwfaoro.github.io"
+          target="_blank"
+          className="underline text-blue-500"
+        >
+          portfolio project
+        </a>{' '}
+        was built using Next.js with Static Export, chosen to streamline and
+        simplify the development process. The project was deployed on GitHub
+        Pages, and the entire deployment process is automated using GitHub
+        Actions, ensuring a smooth and continuous delivery pipeline.
+      </p>
+
       <Heading.H2>Main Projects</Heading.H2>
 
       <div className="space-y-4">
